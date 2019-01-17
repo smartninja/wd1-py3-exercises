49f3f9a50448bb8bf508bdc9fbcf263de69be8fe
Matej Ramuta <matej.ramuta@gmail.com>
2019-01-17 12:47:53 -0500

    jinja example changes

--- lesson-16/working-with-jinja/main.py
+++ lesson-16/working-with-jinja/main.py
@@ -1,5 +1,4 @@
 import datetime
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,6 +12,11 @@ def index():
     cities = ["Boston", "Vienna", "Paris", "Berlin"]
 
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+
+@app.route("/about-me")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
