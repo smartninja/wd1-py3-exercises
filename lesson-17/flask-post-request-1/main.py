0435875a7bd6304613fc9a9bd606d992c08f459a
Matej Ramuta <matej.ramuta@gmail.com>
2019-01-18 18:01:15 -0500

    first post request example

---
+++ lesson-17/flask-post-request-1/main.py
@@ -0,0 +1,36 @@
+import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+
+@app.route("/about-me", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
