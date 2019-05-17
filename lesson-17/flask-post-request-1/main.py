62ca95d5c2ffab7f83fdfdb77c0c83df2d44d3bf
Matej Ramuta <matej.ramuta@gmail.com>
2019-05-17 15:04:13 -0400

    lesson 17 exercises updated for Heroku

--- lesson-17/flask-post-request-1/main.py
+++ lesson-17/flask-post-request-1/main.py
@@ -33,4 +33,4 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="5001")
--- lesson-17/guess-secret-number/main.py
+++ lesson-17/guess-secret-number/main.py
@@ -35,4 +35,4 @@ def result():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="5002")
